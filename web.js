c94106a0dbbb15164f074357df0f742a7c21507e
Haru <eg@gmail.com>
2013-07-11 12:10:16 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var content;
     var fs = require('fs'); 
-    fs.readFileSync('./index.html',function read(err, content){
+    fs.readFileSync('index.html',function read(err, content){
 	if (err) throw err;
 	console.log(content);
 	});
